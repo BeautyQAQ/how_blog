e9f7ccd736880c8b195dc1c038b841961b952ad6
huangshen <1426887150@qq.com>
2020-11-15 17:45:33 +0800

    设置网关模块为前台运行，方便查看日志

--- start-all.sh
+++ start-all.sh
@@ -2,7 +2,7 @@
 nohup java -jar how_friend.jar > how_friend.log &
 nohup java -jar how_base.jar > how_base.log &
 nohup java -jar how_article.jar > how_article.log &
-nohup java -jar how_gateway_manager.jar > how_gateway_manager.log &
+nohup java -jar how_gateway_web.jar > how_gateway_web.log &
 nohup java -jar how_gathering.jar > how_gathering.log &
 nohup java -jar how_qa.jar > how_qa.log &
 nohup java -jar how_search.jar > how_search.log &
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-nohup java -jar how_friend.jar > how_friend.log &
-nohup java -jar how_base.jar > how_base.log &
