4597c467a62494abd01e09fa9ea5218589ee85b0
huangshen <1426887150@qq.com>
2020-11-21 18:21:01 +0800

    添加城市接口

--- how_base/src/main/java/com/liushao/controller/LabelController.java
+++ how_base/src/main/java/com/liushao/controller/LabelController.java
@@ -9,14 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 /**
  * @author huangshen
  */
 @RestController
-@RequestMapping("/label")
+@RequestMapping("/base/label")
 public class LabelController {
     @Autowired
     private LabelService labelService;
