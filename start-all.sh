ea826ed2c3406710dd2c707ca2fe762b6e7d590f
huangshen <1426887150@qq.com>
2020-12-02 21:11:43 +0800

    暂时不启动活动模块

--- start-all.sh
+++ start-all.sh
@@ -3,7 +3,7 @@ nohup java -jar how_friend.jar > how_friend.log &
 nohup java -jar how_base.jar > how_base.log &
 nohup java -jar how_article.jar > how_article.log &
 nohup java -jar how_gateway_web.jar > how_gateway_web.log &
-nohup java -jar how_gathering.jar > how_gathering.log &
+#nohup java -jar how_gathering.jar > how_gathering.log &
 nohup java -jar how_qa.jar > how_qa.log &
 nohup java -jar how_search.jar > how_search.log &
 nohup java -jar how_sms.jar > how_sms.log &
