d17e3c7fb210b7a8bba373d28c80f11aaf6ba168
huangshen <1426887150@qq.com>
2021-02-25 14:13:43 +0800

    rabbitmq监听修改设置，默认启动时自动创建队列

---
+++ doc/run.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+echo '=====开始运行后台====='
+
+cd docker-compose
+
+
+echo '=====开始运行how_article====='
+
+docker-compose -f how_article.yml up -d
+
+echo '=====开始运行how_base====='
+
+docker-compose -f how_base.yml up -d
+
+echo '=====开始运行how_friend====='
+
+docker-compose -f how_friend.yml up -d
+
+echo '=====开始运行how_gateway_manager====='
+
+docker-compose -f how_gateway_manager.yml up -d
+
+echo '=====开始运行how_gateway_web====='
+
+docker-compose -f how_gateway_web.yml up -d
+
+echo '=====开始运行how_qa====='
+
+docker-compose -f how_qa.yml up -d
+
+echo '=====开始运行how_sms====='
+
+docker-compose -f how_sms.yml up -d
+
+echo '=====how_spit====='
+
+docker-compose -f how_spit.yml up -d
+
+echo '=====开始运行how_user====='
+
+docker-compose -f how_user.yml up -d
+
+echo '执行完成 日志目录how_blog/doc/log'
