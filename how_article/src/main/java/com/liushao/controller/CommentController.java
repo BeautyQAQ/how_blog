ae61a144105046227dea1a7f016377d9f4a85928
huangshen <1426887150@qq.com>
2021-03-04 18:02:22 +0800

    添加comment评论的条件分页查询

--- how_article/src/main/java/com/liushao/controller/CommentController.java
+++ how_article/src/main/java/com/liushao/controller/CommentController.java
@@ -1,13 +1,18 @@
 package com.liushao.controller;
 
+import com.liushao.entity.PageResult;
 import com.liushao.entity.Result;
 import com.liushao.entity.StatusCode;
+import com.liushao.pojo.Article;
 import com.liushao.pojo.Comment;
 import com.liushao.service.CommentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * 前缀 /article
@@ -21,6 +26,20 @@ public class CommentController {
 
     @Autowired
     private CommentService commentService;
+
+    /**
+     * 分页+多条件查询
+     * @param searchMap 查询条件封装
+     * @param page 页码
+     * @param size 页大小
+     * @return 分页结果
+     */
+    @ApiOperation(value = "分页+多条件查询")
+    @RequestMapping(value="/search/{page}/{size}",method=RequestMethod.POST)
+    public Result findSearch(@RequestBody Map<String, Object> searchMap , @PathVariable int page, @PathVariable int size){
+        Page<Comment> pageList = commentService.findSearch(searchMap, page, size);
+        return  new Result(true,StatusCode.OK,"查询成功",  new PageResult<Comment>(pageList.getTotalElements(), pageList.getContent()) );
+    }
 
     /**
      * 添加评论
--- how_article/src/main/java/com/liushao/service/CommentService.java
+++ how_article/src/main/java/com/liushao/service/CommentService.java
@@ -3,10 +3,12 @@ package com.liushao.service;
 import com.liushao.dao.CommentDao;
 import com.liushao.pojo.Comment;
 import com.liushao.util.IdWorker;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CommentService {
@@ -39,4 +41,39 @@ public class CommentService {
         return commentDao.findByArticleid(articleid);
     }
 
+    /**
+     * 条件分页查询
+     */
+    public Page<Comment> findSearch(Map<String, Object> searchMap, int page, int size) {
+        //条件值对象
+        Comment comment = new Comment();
+        if(StringUtils.isNotEmpty((String)searchMap.get("id"))){
+            comment.set_id((String)searchMap.get("id"));
+        }
+        if(StringUtils.isNotEmpty((String)searchMap.get("articleid"))){
+            comment.set_id((String)searchMap.get("articleid"));
+        }
+        if(StringUtils.isNotEmpty((String)searchMap.get("content"))){
+            comment.set_id((String)searchMap.get("content"));
+        }
+        if(StringUtils.isNotEmpty((String)searchMap.get("userid"))){
+            comment.set_id((String)searchMap.get("userid"));
+        }
+        if(StringUtils.isNotEmpty((String)searchMap.get("parentid"))){
+            comment.set_id((String)searchMap.get("parentid"));
+        }
+        if(StringUtils.isNotEmpty((String)searchMap.get("state"))){
+            comment.set_id((String)searchMap.get("state"));
+        }
+
+        //条件匹配器 如果不设置匹配器默认精确匹配
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching()
+                .withMatcher("content",ExampleMatcher.GenericPropertyMatchers.contains());
+                //.withMatcher("processStatus",ExampleMatcher.GenericPropertyMatchers.exact());
+
+        //定义example条件对象
+        Example<Comment> example = Example.of(comment,exampleMatcher);
+        PageRequest pageRequest =  PageRequest.of(page-1, size);
+        return commentDao.findAll(example, pageRequest);
+    }
 }
